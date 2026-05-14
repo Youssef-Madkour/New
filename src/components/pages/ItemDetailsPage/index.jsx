0bb5bad7d7b253467a0c53dc57ca16273d83438e
Youssef Madkour <youssefmadkour2003@gmail.com>
2026-05-14 22:29:27 +0300

    'update'

--- src/components/pages/ItemDetailsPage/index.jsx
+++ src/components/pages/ItemDetailsPage/index.jsx
@@ -64,34 +64,3 @@ export default ItemDetailsPage;
 
 
 
-
-
-// import { useLocation, useNavigate } from 'react-router-dom';
-// function ItemDetailsPage() {
-//   const location = useLocation();
-//   const navigate = useNavigate();
-//   const { item } = location.state ?? {};
-//   return (
-//     <div className='flex flex-col'>
-//       <button
-//         onClick={() => navigate(-1)}
-//         className='mb-5 w-fit px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer'
-//       >
-//         Back
-//       </button>
-//       {!item ? (
-//         <div className='text-red-500'>Item not found!</div>
-//       ) : (
-//         <>
-//           <img src={item.icon} alt='' className='size-25' />
-//           <h1 className='text-2xl font-bold mt-2'>{item.title}</h1>
-//           <p className='font-semibold - mt-1'>{item.desc}</p>
-//           <p className='mt-2'>
-//             <strong className='font-bold'>Price:</strong> {item.price}
-//           </p>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-// export default ItemDetailsPage;
