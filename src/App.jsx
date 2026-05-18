456cbb093f97efbf58bbfb441fb7118b10c8ef05
Youssef Madkour <youssefmadkour2003@gmail.com>
2026-05-18 20:09:01 +0300

    add new page store

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/common/Layout';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import NotFound from './pages/NotFound';
+import Store from './pages/store';  
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='item/:id' element={<Details />} />
+    <Route path="store" element={<Store />} />
         </Route>
+        
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
