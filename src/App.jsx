27fd35fa07a879b9755ca431a8306a99d1f1f76a
Youssef Madkour <youssefmadkour2003@gmail.com>
2026-05-19 20:23:35 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -23,28 +23,3 @@ function App() {
 export default App;
 
 
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Layout from './components/common/Layout';
-// import Home from './pages/Home';
-// import Details from './pages/Details';
-// import NotFound from './pages/NotFound';
-// import Store from './pages/store';  
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<Layout />}>
-//           <Route index element={<Home />} />
-//           <Route path='item/:id' element={<Details />} />
-//     <Route path="store" element={<Store />} />
-//         </Route>
-        
-//         <Route path='*' element={<NotFound />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
