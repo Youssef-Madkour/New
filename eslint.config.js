a2b1abc2bbc2e17c7b3bad9c57d96c656f5c2add
mahmoud-txg <mahmoud@txglobe.com>
2026-06-06 16:57:47 +0300

    chore: add typescript-eslint naming-convention rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -16,6 +17,43 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          custom: { regex: '^I[A-Z]', match: true },
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+          custom: { regex: '^T[A-Z]', match: true },
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'FunctionDeclaration[id.name=/^[A-Z]/]',
+          message:
+            'React components must be arrow functions: use `const Foo = () => {...}` instead of `function Foo() {...}`.',
+        },
+      ],
     },
   },
 ])
